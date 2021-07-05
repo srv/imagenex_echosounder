d6fabd44a710a060dd9981042bebcf572d818eff
francescbonin <francisco.bonin@uib.es>
2021-07-05 12:01:59 +0200

    change publisher names, add profile as a new parameter, add calculation of n. bytes if profile =1, show data bytes

--- src/imagenex_echosounder_node.cpp
+++ src/imagenex_echosounder_node.cpp
@@ -14,21 +14,24 @@ class imagenex_echosounder {
     // Get configuration from rosparam server
     getConfig();
     ROS_INFO("Range: %f", range);
-	  ROS_INFO("Profunditat min: %f", profundidad_min);
+    ROS_INFO("Profunditat max: %f", range);
+	  ROS_INFO("Profunditat min: %f", profile_minimum_range);
+    ROS_INFO("profile (ON - 1, OFF - 0)): %i", profile);
 	  ROS_INFO("gain: %i", gain);
 	  ROS_INFO("pulse_length: %i", pulse_length);
 	  ROS_INFO("delay: %i", delay);
 	  ROS_INFO("data_points: %i", data_points);
-    ROS_INFO("profile: %i", profile);
     ROS_INFO("timeoutSerial: %i", timeoutSerial);
     ROS_INFO("timerDuration: %f", timerDuration);
     ROS_INFO("absorption: %f", absorption);
     ROS_INFO("range percentage: %f", range_percentage);
     ROS_INFO("sample_vector_size: %i", sample_vector_size);
 
+
     //Publishers
     profile_range_raw_pub_ = nhp_.advertise<sensor_msgs::Range>("profile_range_raw", 1); 
     data_bytes_raw_pub_ = nhp_.advertise<sensor_msgs::Range>("data_bytes_raw", 1);
+    profile_range_filtered_pub_ = nhp_.advertise<sensor_msgs::Range>("profile_range_filtered", 1); 
 
     serial.open(115200); 
 	  serial.setTimeout(boost::posix_time::seconds(timeoutSerial)); 
@@ -43,7 +46,7 @@ class imagenex_echosounder {
   {      
     //profile_minimum_range
     if(profile_minimum_range < 0) profile_minimum_range = 0;
-    else if (profile_minimum_range > 250) profile_minimum_range = 250; 
+    else if (profile_minimum_range > 25.0) profile_minimum_range = 25.0; 
 
     //gain        
     if(gain > 40) gain = 40;
@@ -55,8 +58,10 @@ class imagenex_echosounder {
 	  else if (pulse_length < 1) pulse_length = 1;
 
     //Set the buffer_rx bytes number
-    if (data_points==25 & profile==1) unsigned char buffer_rx[265];
-    else (data_points==50 & profile==1) unsigned char buffer_rx[513];
+    if (data_points==25 && profile==1) unsigned char buffer_rx[265];
+    else (data_points==50 && profile==1) unsigned char buffer_rx[513]; // be carefull !!! a logical and is: &&
+
+    if(delay/2 == 253) delay = 508; // all values equal to 253 have to be filtered out. 253 is reserved for end of frame.
 
     unsigned char buffer_tx[27];
       
@@ -97,13 +102,32 @@ class imagenex_echosounder {
     {
     }
 
-    profile_range_high_byte = float((buffer_rx[9] & 0x7E) >> 1);
+    /*profile_range_high_byte = float((buffer_rx[9] & 0x7E) >> 1);
     profile_range_low_byte = float((buffer_rx[9] & 0x01) << 7)|(buffer_rx[8] & 0x7F));
     profile_range = 0.01 *float((profile_range_high_byte << 8)|profile_range_low_byte);
 
     data_bytes_high_byte = float((buffer_rx[11] & 0x7E) >> 1);
     data_bytes_low_byte = float((buffer_rx[11] & 0x01) << 7)|(buffer_rx[10] & 0x7F));
-    data_bytes = float((data_bytes_high_byte << 8)|data_bytes_low_byte);
+    data_bytes = float((data_bytes_high_byte << 8)|data_bytes_low_byte);*/
+
+
+
+    ROS_INFO("ASCII M or G ? : %c", buffer_rx[1]);
+   
+    if (buffer_rx[1]=='M' or buffer_rx[1]=='G') 
+    {
+      DataBytes12=static_cast<unsigned int>(buffer_rx[12]);
+      DataBytes13=static_cast<unsigned int>(buffer_rx[13]);
+      DataBytes14=static_cast<unsigned int>(buffer_rx[14]);
+      DataBytes15=static_cast<unsigned int>(buffer_rx[15]);
+      DataBytes16=static_cast<unsigned int>(buffer_rx[16]);
+
+      ROS_INFO("Data Bytes: %i %i %i %i %i ", DataBytes12, DataBytes13, DataBytes14, DataBytes15, DataBytes16);
+    }
+
+    profile_range = 0.01 * float(((buffer_rx[9] & 0x7F) << 7) | (buffer_rx[8] & 0x7F)); // this decodification is equal to the one specified in the datasheet
+    data_bytes = float(((buffer_rx[11] & 0x7F) << 7) | (buffer_rx[10] & 0x7F)); // this decodification is equal to the one specified in the datasheet
+    
 
 
     // Publish data_bytes raw measurement
@@ -116,7 +140,7 @@ class imagenex_echosounder {
     msg.max_range = range;
     msg.range = data_bytes;
     // Filter zeros
-    if(data_bytes >= profile_minimum_range){
+    if(profile == 1){
       data_bytes_raw_pub_.publish(msg);
     }
     
@@ -133,17 +157,68 @@ class imagenex_echosounder {
     if(profile_range >= profile_minimum_range){
       profile_range_raw_pub_.publish(msg);
     }
+    else{ROS_WARN("profile range not published, it is lower than profile minimum range: %i");}
   
+  double diff_ranges = (fabs(profile_range - previous_profile_range) * 100)/(std::max(profile_range,previous_profile_range)); // in percentage of the highest value
+
+
+  if(sample_counter <= sample_vector_size){ // vector of samples to check if the sensor gets lost (range= 0.00) during a number of samples. 
+      sample_vector.push_back(depth); 
+      sample_counter++;
+      average = accumulate( sample_vector.begin(), sample_vector.end(), 0.0) / sample_vector_size; // computes de mean. 
+      if (average ==0.00){  // got lost during sample_vector_size sensor samples. All samples = 0.00. IMPORTANT: Sample = 0 means that the ecosounder is lost or OUT OF RANGE.  
+        gotlost = true; // this variable is set after sample_vector_size samples equal to 0 have been detected.
+        ROS_WARN("Echosounder Got Lost during: %i samples", sample_vector_size);
+      }
+    }
+    if(sample_counter > sample_vector_size) sample_counter=0;
+      
+    // got lost if 10 consecutive samples =0 
+/* verify the diff_ranges respect the profundidad and profundidad_anterior and why */
+
+    if (!gotlost and (diff_ranges<range_percentage) and (profile_range != 0) and (profile_range <= range) and (profile_range > profile_minimum_range)) 
+// if the echosounder is not lost, and the difference in range between consecutive samples, and "profundidad" fullfills all requirements , publish the "filtered" sample. 
+// IMPORTANT: do not consider altitudes of exactly 0.5 since they are outliers caused when the sensor approximates to its lowest range. DEPTHS LOWER THAN 0.5 ARE NOT RELIABLE. MINIMUM RANGE DETECTABLE = 0.5 M
+    {
+      profile_range_filtered_pub_.publish(msg);
+      ////ROS_WARN("altitude filtered published");
+      previous_profile_range=profile_range; // updating the value of this variable only when the message is published 
+      //avoids the possibility of having 2 or 3 consecutive outliers, assuming that the vehicle altitude will not change abruptly   
+    } // on the other hand, if the sensor gets lost (value ==> 0.00) during a certain period, the new data could be 
+      // far from the last stored and valid range, and this will prevent the publication of further range samples WHEN IT RECOVERS. So, lets publish new range data after the sensor recovers. 
+// how will we know that the sensor has recovered ? it gotlost ("profundidad = 0"), but the "profundidad" is again <> 0 (recovered !!!), and in the range of accepted values. 
+    if (gotlost and (profile_range != 0) and (profile_range <= range) and (profile_range >= profile_minimum_range)) 
+    {
+      profile_range_filtered_pub_.publish(msg); // publish next range after recovered 
+      previous_profile_range=profile_range; // initialize the value of profundidad_anterior with the last published range
+      gotlost=false;    
+    }
+    
+  } // an initialization proceduce in case of loss is needed to prevent this latter case.
+
+  
+
+
+
   ros::NodeHandle nh_;
   ros::NodeHandle nhp_;
-  ros::Publisher altitude_raw_pub_;
-  ros::Publisher altitude_filtered_pub_;
+  ros::Publisher profile_range_raw_pub_;
+  ros::Publisher profile_range_filtered_pub_;
   ros::Timer timer_;
   int64_t seq_;
   TimeoutSerial serial;
-  double profile_minimum_range, profile_range_high_byte, profile_range_low_byte, profile_range, old_profile_range,range;
+  double depth, previous_depth;
+  double profile_minimum_range, profile_range_high_byte, profile_range_low_byte, profile_range, old_profile_range,range, previous_profile_range;
   double data_bytes_high_byte, data_bytes_low_byte, data_bytes,timerDuration;
   int gain, absorption,long_pulso,delay,data_points,profile,timeoutSerial; 
+  int DataBytes12, DataBytes13, DataBytes14, DataBytes15, DataBytes16;
+  std::vector<double> sample_vector; 
+  int sample_counter=0;
+  double average, range_percentage; 
+  bool gotlost = false;
+  int sample_vector_size;
+
+
 
   void getConfig() {
     bool valid_config = true;
@@ -158,6 +233,8 @@ class imagenex_echosounder {
     valid_config = valid_config && ros::param::getCached("~timeoutSerial", timeoutSerial);
     valid_config = valid_config && ros::param::getCached("~timerDuration", timerDuration);
     valid_config = valid_config && ros::param::getCached("~absorption", absorption);
+    valid_config = valid_config && ros::param::getCached("~range_percentage", range_percentage);
+    valid_config = valid_config && ros::param::getCached("~sample_vector_size", sample_vector_size);
 
     // Shutdown if not valid
     if (!valid_config) {
